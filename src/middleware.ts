0c17cdb88ed043c5388b55a03c64a4a089809bde
Alexandru Pop <alexpop89@users.noreply.github.com>
2023-10-11 18:44:16 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import {createI18nMiddleware} from 'next-international/middleware';
 import {NextRequest} from 'next/server';
 
 const I18nMiddleware = createI18nMiddleware({
-    locales: ['en', 'fr'],
+    locales: ['en', 'de'],
     defaultLocale: 'en',
     urlMappingStrategy: 'rewrite'
 });
